066d12aa6de1888829362f44d0c2c1421ca59ad0
Krikunov Oleg <okrikunof@gmail.com>
2024-08-05 23:20:59 +0300

    made VLC coding algorithm

---
+++ lib/vlc/vlc.go
@@ -0,0 +1,179 @@
+package vlc
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+type BinaryChunk string
+
+type BinaryChunks []BinaryChunk
+
+type HexChunk string
+
+type HexChunks []HexChunk
+
+type encodingTable map[rune]string
+
+const chunkSize = 8
+
+func Encode(str string) string {
+	str = prepareText(str)
+
+	chunks := splitByChunks(encodeBin(str), chunkSize)
+
+	return chunks.ToHex().ToString()
+}
+
+func (hcs HexChunks) ToString() string {
+	const sep = " "
+
+	switch len(hcs) {
+	case 0:
+		return ""
+	case 1:
+		return string(hcs[0])
+	}
+
+	var buf strings.Builder
+
+	buf.WriteString(string(hcs[0]))
+
+	for _, hc := range hcs[1:] {
+		buf.WriteString(sep)
+		buf.WriteString(string(hc))
+	}
+
+	return buf.String()
+}
+
+func (bcs BinaryChunks) ToHex() HexChunks {
+	res := make(HexChunks, 0, len(bcs))
+
+	for _, chunk := range bcs {
+		hexChunk := chunk.ToHex()
+
+		res = append(res, hexChunk)
+	}
+
+	return res
+}
+
+func (bc BinaryChunk) ToHex() HexChunk {
+	num, err := strconv.ParseUint(string(bc), 2, chunkSize)
+	if err != nil {
+		panic("can't convert binary chunks to hex chunks" + err.Error())
+	}
+
+	res := strings.ToUpper(fmt.Sprintf("%x", num))
+
+	if len(res) == 1 {
+		res = "0" + res
+	}
+
+	return HexChunk(res)
+}
+
+func splitByChunks(bStr string, chunkSize int) BinaryChunks {
+	strLen := utf8.RuneCountInString(bStr)
+
+	chunksCount := strLen / chunkSize
+
+	if strLen/chunkSize != 0 {
+		chunksCount++
+	}
+
+	res := make(BinaryChunks, 0, chunksCount)
+
+	var buf strings.Builder
+
+	for i, ch := range bStr {
+		buf.WriteString(string(ch))
+
+		if (i+1)%chunkSize == 0 {
+			res = append(res, BinaryChunk(buf.String()))
+			buf.Reset()
+		}
+	}
+
+	if buf.Len() != 0 {
+		lastChunk := buf.String()
+
+		lastChunk += strings.Repeat("0", chunkSize-len(lastChunk))
+
+		res = append(res, BinaryChunk(lastChunk))
+	}
+
+	return res
+}
+
+func encodeBin(str string) string {
+	var buf strings.Builder
+	for _, ch := range str {
+		buf.WriteString(bin(ch))
+	}
+
+	return buf.String()
+}
+
+func bin(ch rune) string {
+	table := getEncodingTable()
+
+	res, ok := table[ch]
+	if !ok {
+		panic("unknown character" + string(ch))
+	}
+
+	return res
+}
+
+func getEncodingTable() encodingTable {
+	return encodingTable{
+		' ': "11",
+		'e': "101",
+		't': "1001",
+		'o': "10001",
+		'n': "10000",
+		'a': "011",
+		's': "0101",
+		'i': "01001",
+		'r': "01000",
+		'h': "0011",
+		'd': "00101",
+		'l': "001001",
+		'!': "001000",
+		'u': "00011",
+		'c': "000101",
+		'f': "000100",
+		'm': "000011",
+		'p': "0000101",
+		'g': "0000100",
+		'w': "0000011",
+		'b': "0000010",
+		'y': "0000001",
+		'v': "00000001",
+		'j': "000000001",
+		'k': "0000000001",
+		'x': "00000000001",
+		'q': "000000000001",
+		'z': "000000000000",
+	}
+}
+
+func prepareText(str string) string {
+	var buf strings.Builder
+
+	for _, ch := range str {
+		if unicode.IsUpper(ch) {
+			buf.WriteRune('!')
+			buf.WriteRune(unicode.ToLower(ch))
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return buf.String()
+}
---
+++ lib/vlc/vlc_test.go
@@ -0,0 +1,118 @@
+package vlc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_encodeBin(t *testing.T) {
+	tests := []struct {
+		name string
+		bStr string
+		want string
+	}{
+		{
+			name: "base test",
+			bStr: "!xyz",
+			want: "001000000000000010000001000000000000",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := encodeBin(tt.bStr); got != tt.want {
+				t.Errorf("encodeBin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_splitByChunks(t *testing.T) {
+	type args struct {
+		bStr      string
+		chunkSize int
+	}
+	tests := []struct {
+		name string
+		args args
+		want BinaryChunks
+	}{
+		{
+			name: "base test",
+			args: args{
+				bStr:      "001000000000000010000001000000000000",
+				chunkSize: 8,
+			},
+			want: BinaryChunks{"00100000", "00000000", "10000001", "00000000", "00000000"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := splitByChunks(tt.args.bStr, tt.args.chunkSize); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("splitByChunks() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBinaryChunks_ToHex(t *testing.T) {
+	tests := []struct {
+		name string
+		bcs  BinaryChunks
+		want HexChunks
+	}{
+		{
+			name: "base test",
+			bcs:  BinaryChunks{"0101111", "10000000"},
+			want: HexChunks{"2F", "80"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.bcs.ToHex(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToHex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEncoe1(t *testing.T) {
+	tests := []struct {
+		name string
+		str  string
+		want string
+	}{
+		{
+			name: "base test",
+			str:  "My name is Ted",
+			want: "20 30 3C 18 77 4A E4 4D 28",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Encode(tt.str); got != tt.want {
+				t.Errorf("Encode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_prepareText(t *testing.T) {
+	tests := []struct {
+		name string
+		str  string
+		want string
+	}{
+		{
+			name: "base test",
+			str:  "My name is Ted",
+			want: "!my name is !ted",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := prepareText(tt.str); got != tt.want {
+				t.Errorf("prepareText() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

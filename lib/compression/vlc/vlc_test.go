aa74bf25ad9f67affb7f9ab4b5fdf84a402d8217
Krikunov Oleg <okrikunof@gmail.com>
2024-08-10 13:31:42 +0300

    refactor structure

--- lib/compression/vlc/vlc_test.go
+++ lib/compression/vlc/vlc_test.go
@@ -40,7 +40,8 @@ func TestEncode(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Encode(tt.str); !reflect.DeepEqual(got, tt.want) {
+			encoder := New()
+			if got := encoder.Encode(tt.str); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Encode() = %v, want %v", got, tt.want)
 			}
 		})
@@ -103,7 +104,8 @@ func TestDecode(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Decode(tt.encodedText); got != tt.want {
+			decoder := New()
+			if got := decoder.Decode(tt.encodedText); got != tt.want {
 				t.Errorf("Decode() = %v, want %v", got, tt.want)
 			}
 		})
